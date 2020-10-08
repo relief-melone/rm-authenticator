653e573451cd4f8026331172f7fbeea3283b055c
Relief Melone <relief.melone@gmail.com>
2020-10-08 14:10:58 +0200

    added debug mode

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,12 @@
 BASEDIR=$(dirname $0)
-echo "Starting Script: $RUN_SCRIPT"
+echo "Current Environment: $NODE_ENV"
 
 if [[ $NODE_ENV == "development" ]]; then
   echo "RUNNING SERVER IN DEVELOPMENT MODE"
   npm run dev
+else if [[ $NODE_ENV == "debug" ]]; then
+  echo "RUNNING SERVER IN DEBUG MODE"
+  npm run debug
 else
   echo "RUNNING SERVER IN PRODUCTION MODE"
   npm run start
