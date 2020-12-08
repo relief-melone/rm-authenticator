4265ae0016de4c070306f51df2152800fbf8f4cc
Relief Melone <relief.melone@gmail.com>
2020-12-08 10:07:49 +0100

    added ression destory

--- src/controller/controller.auth.logout.ts
+++ src/controller/controller.auth.logout.ts
@@ -5,5 +5,9 @@ export default (req: Request, res: Response, next: NextFunction): void => {
   req.logout();
   if(!req.session)return res.redirect(configMain.applicationLogoutURL);
 
-  return res.redirect(req.session.redirect.logout);  
+  const logout = req.session.redirect.logout;
+  req.session.destroy(err => {
+    return res.redirect(logout);  
+  });
+  
 };
